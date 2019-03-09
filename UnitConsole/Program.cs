af299e80d72cc8742c9837417c7994aaef34bee3
Hasan Abrar <hasan.abrar@outlook.co.nz>
2019-03-10 12:42:55 +1300

    unit class

--- UnitConsole/Program.cs
+++ UnitConsole/Program.cs
@@ -6,7 +6,68 @@ namespace UnitConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the Unit to convert!");
+            Console.WriteLine("Enter 1 for Temprature, 2 for Volume");
+            string unitMain = Console.ReadLine();
+            string unitFrom = "";
+            string unitTo = "";
+            string unitVal = "0";
+
+            try
+            {
+                if (unitMain == "1")
+                {
+                    Console.WriteLine("Enter From Unit like one of from Fahrenheit/Celsius/Kelvin ");
+                    unitFrom = Console.ReadLine();
+
+                    Console.WriteLine("Enter To Unit like one of from Fahrenheit/Celsius/Kelvin");
+                    unitTo = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter Unit value like greater than 0");
+                    unitVal = Console.ReadLine();
+
+
+                    Units unitObj = new Temperature();
+                    double results = unitObj.ConvertUnit(double.Parse(unitVal), unitFrom.ToLower(), unitTo.ToLower());
+
+                    Console.WriteLine("{0} ({1}) = {2} ({3})", unitFrom, unitVal, unitTo, results.ToString());
+                    Console.ReadLine();
+                }
+                else if (unitMain == "2")
+                {
+                    Console.WriteLine("Enter From Unit like one of from Litre/Millilitre/USgal");
+                    unitFrom = Console.ReadLine();
+
+                    Console.WriteLine("Enter To Unit like one of from Litre/Millilitre/USgal");
+                    unitTo = Console.ReadLine();
+
+                    Console.WriteLine("Enter Unit value like greater than 0");
+                    unitVal = Console.ReadLine();
+
+
+                    Units unitObj = new Volume();
+                    double results = unitObj.ConvertUnit(double.Parse(unitVal), unitFrom.ToLower(), unitTo.ToLower());
+
+                    Console.WriteLine("{0} ({1}) = {2} ({3})", unitFrom, unitVal, unitTo, results.ToString());
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Unit");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Invalid operation");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+            
+
+
         }
     }
 }
---
+++ UnitConsole/Units.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitConsole
+{
+    abstract public class Units
+    {
+        double unitValue { get; set; }
+        public abstract double ConvertUnit(double unitVal, string unitFromType, string unitToType);
+    }
+
+    public class Temperature : Units
+    {
+
+        public override double ConvertUnit(double unitVal, string unitFromType, string unitToType)
+        {
+            
+            var dicTemp = new Dictionary<string, Delegate>();
+                                    
+            dicTemp.Add("fahrenheitTocelsius", new Func<double, double>(FahrenheitToCelsius));
+            dicTemp.Add("celsiusTofahrenheit", new Func<double, double>(CelsiusToFahrenheit));
+            dicTemp.Add("kelvinTofahrenheit", new Func<double, double>(KelvinToFahrenheit));
+            dicTemp.Add("fahrenheitTokelvin", new Func<double, double>(FahrenheitToKelvin));
+            dicTemp.Add("kelvinTocelsius", new Func<double, double>(KelvinToCelsius));
+            dicTemp.Add("celsiusTokelvin", new Func<double, double>(CelsiusToKelvin));
+
+            
+            string keyValue = unitFromType + "To" + unitToType;
+            var results = dicTemp[keyValue].DynamicInvoke(unitVal);
+            return double.Parse(results.ToString());
+            
+        }
+
+        public static double FahrenheitToCelsius(double unitVal)
+        {
+            return (unitVal - 32) * 5 / 9;
+        }
+        public static double CelsiusToFahrenheit(double unitVal)
+        {
+            return (unitVal * 9 / 5) + 32;
+        }
+        public static double KelvinToFahrenheit(double unitVal)
+        {
+            return (unitVal - 273.15) * 9 / 5 + 32;
+        }
+        public static double FahrenheitToKelvin(double unitVal)
+        {
+            return (unitVal - 32) * 5 / 9 + 273.15;
+        }
+        public static double KelvinToCelsius(double unitVal)
+        {
+            return unitVal - 273.15;
+        }
+        public static double CelsiusToKelvin(double unitVal)
+        {
+            return unitVal + 273.15;
+        }
+
+    }
+    public class Volume : Units
+    {
+        public override double ConvertUnit(double unitVal, string unitFromType, string unitToType)
+        {
+            
+            var dicTemp = new Dictionary<string, Delegate>();
+
+            dicTemp.Add("litreTomillilitre", new Func<double, double>(LitreToMillilitre));
+            dicTemp.Add("milliLitreTolitre", new Func<double, double>(MilliLitreToLitre));
+            dicTemp.Add("usgalTolitre", new Func<double, double>(USGalToLitre));
+            dicTemp.Add("litreTousgal", new Func<double, double>(LitreToUSGal));
+            dicTemp.Add("usgalTomillilitre", new Func<double, double>(USGalToMillilitre));
+            dicTemp.Add("millilitreTousgal", new Func<double, double>(MillilitreToUSGal));
+
+
+            string keyValue = unitFromType + "To" + unitToType;
+            var results = dicTemp[keyValue].DynamicInvoke(unitVal);
+            return double.Parse(results.ToString());
+
+        }
+
+        public double LitreToMillilitre(double unitVal)
+        {
+            return unitVal * 1000;
+        }
+        public double MilliLitreToLitre(double unitVal)
+        {
+            return (unitVal / 1000);
+        }
+        public double USGalToLitre(double unitVal)
+        {
+            return unitVal * 3.785;
+        }
+        public double LitreToUSGal(double unitVal)
+        {
+            return unitVal / 3.785;
+        }
+        public double USGalToMillilitre(double unitVal)
+        {
+            return unitVal * 3785.412;
+        }
+        public double MillilitreToUSGal(double unitVal)
+        {
+            return unitVal / 3785.412;
+        }
+
+    }
+}
