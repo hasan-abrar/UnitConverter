eadf8be98f5e07add263124dc2b9002df076fd1f
Hasan Abrar <hasan.abrar@outlook.co.nz>
2019-03-10 12:40:04 +1300

    assignment

---
+++ UnitConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
